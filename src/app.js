8184d03b45a64f3afcc214348bc86ac251196716
Huzaifa0007 <khanhuzaifa1010@gmail.com>
2026-01-03 17:18:36 +0530

    fix vercel express config

--- src/app.js
+++ src/app.js
@@ -6,6 +6,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// ✅ HEALTH CHECK
+app.get("/", (req, res) => {
+  res.json({
+    status: "Backend is running 🚀",
+    api: "/api/movies",
+  });
+});
+
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/movies", require("./routes/movieRoutes"));
 
