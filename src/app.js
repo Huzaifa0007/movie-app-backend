92b8874384b84c2485e8303a8e4d610ce7930670
Huzaifa0007 <khanhuzaifa1010@gmail.com>
2026-01-03 17:10:07 +0530

    fix vercel express config

--- src/app.js
+++ src/app.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-const errorHandler = require("./middleware/errorMiddleware");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/movies", require("./routes/movieRoutes"));
 
-// error middleware MUST be last
+const errorHandler = require("./middleware/errorMiddleware");
 app.use(errorHandler);
 
 module.exports = app;
